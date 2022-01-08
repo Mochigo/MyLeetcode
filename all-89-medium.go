1d6f1a673956073dfd448b60748f067c45981796
Mochigo <2391542095@qq.com>
2022-01-08 11:37:27 +0800

    :art: 2022.1.8 everday leetcode question

--- all-89-medium.go
+++ all-89-medium.go
@@ -1,10 +1,20 @@
 package main
 
-func grayCode(n int) []int {
+func grayCode1(n int) []int {
 	ans := make([]int, 1<<n)
 	for i := range ans {
 		ans[i] = i>>1 ^ i
 	}
 
+	return ans
+}
+
+func grayCode2(n int) []int {
+	ans := make([]int, 1, 1<<n)
+	for i := 1; i <= n; i++ {
+		for j := len(ans) - 1; j >= 0; j-- {
+			ans = append(ans, ans[j]|(1<<(i-1)))
+		}
+	}
 	return ans
 }
